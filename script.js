4b23c94de402b1b4ba5086561fc6848fa388cca1
Ayberk Levendoglu <ayberklevendoglu@gmail.com>
2023-12-22 16:06:02 +0300

    movement patterns structure

--- script.js
+++ script.js
@@ -4,23 +4,45 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
-const numberOfEnemies = 100;
+const numberOfEnemies = 50;
 const enemiesArray = [];
+
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = "images/enemy1.png";
+    // this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 3;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
+    //animate sprites
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -35,6 +57,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 
